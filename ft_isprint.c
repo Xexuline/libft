cc0981e4ac70a4119d373dce6cfc56e2c60d3fd4
Jesus Sabroso Centella <jsabroso@c2r3s1.42malaga.com>
2024-09-14 11:30:43 +0200

    Add isprintable

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*   Created: 2024/09/14 11:29:05 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/14 11:30:12 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isdigit(int n);
-int	ft_isalpha(int n);
-int	ft_isalnum(int n);
-int	ft_isascii(int n);
-int	ft_isprint(int n);
-
-#endif
+int ft_isprint(int n)
+{
+	if (n >= 32 && n <= 126)
+		return (1);
+	return (0);
+}
