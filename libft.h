2d75b6b99fa60f7e8aad93e3a4854b0907862839
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-14 17:39:47 +0200

    Add ft_tolower function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*   Updated: 2024/09/14 17:39:32 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ int		ft_isascii(int n);
 int		ft_isprint(int n);
 int		ft_strlen(char *str);
 int		ft_toupper(int c);
+int		ft_tolower(int c);
 void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
