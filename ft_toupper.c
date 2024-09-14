4b01fc6e4e2e16aed0f84e94e78152bce735eec1
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-14 17:38:11 +0200

    add toupper func

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*   Created: 2024/09/14 17:35:49 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/14 17:37:50 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
-
-int		ft_isdigit(int n);
-int		ft_isalpha(int n);
-int		ft_isalnum(int n);
-int		ft_isascii(int n);
-int		ft_isprint(int n);
-int		ft_strlen(char *str);
-int		ft_toupper(int c);
-void	*ft_memset(void *s, int c, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-
-#endif
+int	ft_toupper(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (c - 32);
+	return (c);
+}
