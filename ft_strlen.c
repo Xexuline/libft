548ec85674fbe8d4879ac32359a75ee9f281bb2b
Jesus Sabroso Centella <jsabroso@c2r3s1.42malaga.com>
2024-09-14 11:37:04 +0200

    add strlen

--- ft_strlen.c
+++ ft_strlen.c
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*   Created: 2024/09/14 11:31:12 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/14 11:31:24 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+int	ft_strlen(char *str)
+{
+	int	i;
 
-int	ft_isdigit(int n);
-int	ft_isalpha(int n);
-int	ft_isalnum(int n);
-int	ft_isascii(int n);
-int	ft_isprint(int n);
-int	ft_strlen(char *str);
-
-#endif
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
