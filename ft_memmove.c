027036f124e4c5f056e2762b1e984b31122f68cd
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-14 17:07:38 +0200

    Add ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,27 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   memmove.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*   Created: 2024/09/14 16:51:21 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/14 16:51:21 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <stdlib.h>
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dest_char;
+	unsigned char	*src_char;
+	unsigned char	*aux;
+	size_t			i;
 
-int		ft_isdigit(int n);
-int		ft_isalpha(int n);
-int		ft_isalnum(int n);
-int		ft_isascii(int n);
-int		ft_isprint(int n);
-int		ft_strlen(char *str);
-void	*ft_memset(void *s, int c, size_t len);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-
-#endif
+	dest_char = (char *)dest;
+	src_char = (char *)src;
+	aux = malloc(sizeof(char));
+	i = 0;
+	while (i < n)
+	{
+		aux[i] = src_char[i];
+		dest_char[i] = aux[i];
+		i++;
+	}
+	free(aux);
+	return (dest);
+}
