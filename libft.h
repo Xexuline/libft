b635f357c5de36741bdad13c2e4d814e9104bdb5
Jesus Sabroso Centella <jsabroso@c2r3s1.42malaga.com>
2024-09-14 11:03:38 +0200

    add basic header

---
+++ libft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/14 10:23:39 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/14 10:23:39 by jsabroso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isdigit(int n);
+int	ft_isalpha(int n);
+int	ft_isalnum(int n);
+int	ft_isascii(int n);
+
+#endif
