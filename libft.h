4e1137a8b0458ea32a61c65d4bf99fc0fca2e8a3
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-15 14:25:57 +0200

    Add header to libft

--- libft.h
+++ libft.h
@@ -13,6 +13,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 # include <stdlib.h>
+// Remove next headers
+# include <stdio.h>
 
 int		ft_isdigit(int n);
 int		ft_isalpha(int n);
@@ -26,5 +28,6 @@ void	*ft_memset(void *s, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dst, const char *src, size_t len);
+size_t	ft_strlcat(char *dst, const char *src, size_t len);
 
 #endif
