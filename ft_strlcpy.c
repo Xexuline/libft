e460bc1c0b8250fc935d0b742844b7e066e1f36b
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-15 12:38:58 +0200

    Add strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 12:34:55 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/15 12:34:55 by jsabroso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+// Copies len - 1 chars from src to dst, and adds a null terminator
+void	ft_strlcpy(char *dst, const char *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] && i < len - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+}
