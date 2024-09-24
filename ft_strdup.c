68f413644769fa9898cec23ff413a052b46582d1
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-09-24 18:01:34 +0200

    WIP strdup

---
+++ ft_strdup.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 17:51:51 by jsabroso          #+#    #+#             */
+/*   Updated: 2024/09/24 17:51:51 by jsabroso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	size_t size;
+	size_t i;
+	char *str;
+
+	i = 0;
+	size = ft_strlen(s);
+	str = malloc((size * sizeof(char)) + 1);
+	if (!str)
+		return (NULL);
+	while (i++ < size)
+		str[i] = s[i];
+	return (str);
+}
--- libft.h
+++ libft.h
@@ -37,5 +37,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t len);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strdup(const char *s);
 
 #endif
