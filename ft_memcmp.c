ec6a5f4c0f3b3322541137dcd59199d15392e355
jsabroso <jsabroso@student.42malaga.com>
2024-10-12 12:35:04 +0200

    Fix memmcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -23,7 +23,11 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	str = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n && str[i] && str2[i] && str[i] == str2[i])
+	while (i < n)
+	{
+		if (str[i] != str2[i])
+			return (str[i] - str2[i]);
 		i++;
-	return (str[i] - str2[i]);
+	}
+	return (0);
 }
