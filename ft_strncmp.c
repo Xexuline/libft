4c8f80e347b3f14120e354ae1dd796e5b0fd1ca8
jsabroso <jsabroso@student.42malaga.com>
2024-10-12 17:02:05 +0200

    Fix strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jsabroso <jsabroso@student.42malaga.c>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 19:45:53 by jsabroso          #+#    #+#             */
-/*   Updated: 2024/09/23 19:45:53 by jsabroso         ###   ########.fr       */
+/*   Updated: 2024/10/12 17:01:40 by jsabroso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (!n)
+		return (0);
 	i = 0;
 	while (i < n && s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	if (i == n)
 		i--;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
