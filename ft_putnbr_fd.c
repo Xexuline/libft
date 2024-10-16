35f9baa7205c1243165f44f343c9e3ad5e67d4ab
jsabroso <jsabroso@student.42malaga.com>
2024-10-16 20:43:30 +0200

    move putnbr from itoa to recursivity

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -15,8 +15,19 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*str;
-
-	str = ft_itoa(n);
-	ft_putstr_fd(str, fd);
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n = -n;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+		ft_putchar_fd(n + '0', fd);
 }
