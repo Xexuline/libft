8eb53e78bb9107b998f840f8bcd7a2fa5c453889
Jesus Sabroso <jsabroso@student.42malaga.com>
2024-10-21 13:48:36 +0200

    remove content if list is not created

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_lst;
 	t_list	*new_elem;
+	void	*new_content;
 
 	if (!lst || !f)
 		return (NULL);
 	new_lst = NULL;
 	while (lst)
 	{
-		new_elem = ft_lstnew(f(lst->content));
+		new_content = f(lst->content);
+		new_elem = ft_lstnew(new_content);
 		if (!new_elem)
 		{
+			if (new_content)
+				del(new_content);
 			ft_lstclear(&new_lst, del);
 			return (NULL);
 		}
